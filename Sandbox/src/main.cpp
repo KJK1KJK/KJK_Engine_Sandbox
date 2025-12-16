88d586c4614c69dd128416a41581e35a073d48ca
KJK1KJK <83895246+KJK1KJK@users.noreply.github.com.>
2025-12-16 20:38:09 +0100

    Divided the project between the engine and test space.

---
+++ Sandbox/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	//Main game Loop
+	bool quit = false;
+	while (!quit)
+	{
+		std::cout << "Hello, Sandbox!" << std::endl;
+		system("cls");
+	}
+
+	return 0;
+}
