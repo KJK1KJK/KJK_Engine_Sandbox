96f29b6b23649e75ca95ce1c151254b5bc41e397
KJK1KJK <83895246+KJK1KJK@users.noreply.github.com.>
2025-12-18 20:45:39 +0100

    Created a Playground folder for testing external library features before implementation.

--- Engine/src/KJK_Engine/Core/EntryPoint.h
+++ Engine/src/KJK_Engine/Core/EntryPoint.h
@@ -4,10 +4,13 @@
 
 #ifdef KJK_PLATFORM_WINDOWS
 
-#include "KJK_Engine/Application.h"
-#include "KJK_Engine/Logger.h"
+#include "KJK_Engine/Core/Application.h"
+#include "KJK_Engine/Core/Logger.h"
 
+//Get the application created in the client code
 extern KJK::Application* KJK::CreateApplication();
+
+#ifndef TEST_NO_ENTRYPOINT
 
 int main(int argc, char** argv)
 {
@@ -19,5 +22,7 @@ int main(int argc, char** argv)
 	app->Run();
 	delete app;
 }
+
+#endif // TEST_NO_ENTRYPOINT
 
 #endif // KJK_PLATFORM_WINDOWS
---
+++ Playground/src/main.cpp
@@ -0,0 +1,18 @@
+//File for testing external library features before implementing them in the engine
+#include <KJK>
+#include <iostream>
+
+
+#include <glad/glad.h>
+
+
+
+int main()
+{
+	KJK::Logger::Init();
+	KJK_INFO("Start the Playground!");
+
+
+
+	return 0;
+}
