ddf588c5fec8043aa8f284ab986e92519e284c13
KJK1KJK <83895246+KJK1KJK@users.noreply.github.com.>
2025-12-25 19:52:53 +0100

    Window created using sdl3 and opengl. Following this tutiorials: https://learnopengl.com/Getting-started/Hello-Window

--- Playground/src/main.cpp
+++ Playground/src/main.cpp
@@ -1,84 +1,95 @@
 //File for testing external library features before implementing them in the engine
 #include <KJK>
 
-#include <SDL3/SDL.h> //Contains the SDL datatypes and functions
-#include <SDL3/SDL_main.h> //Defines main for SDL applications
+#include <glad/glad.h>
 
-//Screen dimension constants
-constexpr int kScreenWidth{ 640 };
-constexpr int kScreenHeight{ 480 };
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+//Initializes the logging system
+void initLogger();
+
+//Initializes OpenGl and SDL, then creates a window
+bool init();
+
+//Cleans up and closes SDL and all used objects
+void close();
+
+//Global variables
+constexpr int SCREEN_WIDTH{ 800 };
+constexpr int SCREEN_HEIGHT{ 600 };
 
 //The window to render to
-SDL_Window* gWindow{ nullptr };
+SDL_Window* gWindow = nullptr;
 
-//The surface contained by the window
-SDL_Surface* gScreenSurface{ nullptr };
-
-//The image to load and show on the screen
-SDL_Surface* gHelloWorld{ nullptr };
-
-void initLogger();
-bool init();
-bool loadMedia();
-void close();
+//Context for OpenGL
+SDL_GLContext gContext;
 
 int main(int argc, char* args[])
 {
 	//Initialize the logging system
 	initLogger();
 
-
 	//Final exit code
 	int exitCode{ 0 };
 
-	//Initialize SDL
+	//Initialize SDL and OpenGL, then create a window
 	if (init() == false)
 	{
-		KJK_ERROR("Failed to initialize!\n");
+		KJK_ERROR("Failed to initialize SDL and OpenGL!");
+		close();
 		exitCode = 1;
 	}
 	else
 	{
-		//Load media
-		if (loadMedia() == false)
-		{
-			KJK_ERROR("Failed to load media!\n");
-			exitCode = 2;
-		}
-		else
-		{
-			//The quit flag
-			bool quit{ false };
+		//Main loop flag
+		bool quit = false;
 
-			//The event data
-			SDL_Event e;
-			SDL_zero(e);
+		//Event handler
+		SDL_Event e;
 
-			//The main loop
-			while (quit == false)
+		//While application is running
+		while (!quit)
+		{
+			//Handle events on queue
+			while (SDL_PollEvent(&e) != 0)
 			{
-				//Handle events on the queue
-				while (SDL_PollEvent(&e) == true)
+				//User requests quit
+				if (e.type == SDL_EVENT_QUIT)
 				{
-					//Check for quit event
-					if (e.type == SDL_EVENT_QUIT)
+					quit = true;
+				}
+				//Window resize event
+				if(e.type == SDL_EVENT_WINDOW_RESIZED)
+				{
+					//Adjust the viewport when the window size changes
+					glViewport(0, 0, e.window.data1, e.window.data2);
+				}
+				//Handle keyboard input
+				else if (e.type == SDL_EVENT_KEY_DOWN)
+				{
+					switch (e.key.key)
 					{
+					case SDLK_ESCAPE:
 						quit = true;
+						break;
+					default:
+						break;
 					}
 				}
-				//Fill the surface white
-				SDL_FillSurfaceRect(gScreenSurface, nullptr, SDL_MapSurfaceRGB(gScreenSurface, 0xFF, 0xFF, 0xFF));
-
-				//Render the image onto the screen surface
-				SDL_BlitSurface(gHelloWorld, nullptr, gScreenSurface, nullptr);
-
-				//Update the surface
-				SDL_UpdateWindowSurface(gWindow);
 			}
+
+			//Set the clear color
+			glClearColor(1.f, 1.f, 1.f, 1.0f);
+			//Clear the color buffer
+			glClear(GL_COLOR_BUFFER_BIT);
+
+			//Update screen
+			SDL_GL_SwapWindow(gWindow);
 		}
 	}
 
-	//Free resources and close SDL
+	//Clean up and close SDL and all used objects
 	close();
 
 	return exitCode;
@@ -91,65 +102,70 @@ static void initLogger()
 	KJK_INFO("Started the Playground!");
 }
 
-//Starts up SDL and creates a window
+//Initializes OpenGl and SDL, then creates a window
 bool init()
 {
-	//Declare success flag
-	bool success{ true };
+	//Initlialize flag
+	bool success = true;
 
 	//Initialize SDL
 	if (SDL_Init(SDL_INIT_VIDEO) == false)
 	{
-		KJK_ERROR("SDL could not initialize! SDL_Error: {}\n", SDL_GetError());
+		KJK_ERROR("Failed to initialize SDL: {0}", SDL_GetError());
 		success = false;
 	}
 	else
 	{
-		//Create window
-		gWindow = SDL_CreateWindow("SDL3 Tutorial: Hello SDL3", kScreenHeight, kScreenHeight, 0);
-		if(gWindow == nullptr)
+		//Use OpenGL 4.5 core
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+		//Create a window
+		gWindow = SDL_CreateWindow("LearnOpenGL", SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
+		if (gWindow == NULL)
 		{
-			KJK_ERROR("Window could not be created! SDL_Error: {}\n", SDL_GetError());
+			KJK_ERROR("Failed to create window: {0}", SDL_GetError());
 			success = false;
 		}
 		else
 		{
-			//Get window surface
-			gScreenSurface = SDL_GetWindowSurface(gWindow);
+			//Create context
+			gContext = SDL_GL_CreateContext(gWindow);
+			if (gContext == NULL)
+			{
+				KJK_ERROR("Failed to create OpenGL context: {0}", SDL_GetError());
+				success = false;
+			}
+			else
+			{
+				//Load OpenGL functions using GLAD
+				if(!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress))
+				{
+					KJK_ERROR("Failed to initialize GLAD! SDL error: {0}", SDL_GetError());
+					success = false;
+				}
+				else
+				{
+					//Define the viewport dimensions
+					glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+				}
+			}
 		}
 	}
 
 	return success;
 }
 
-//Loads media
-bool loadMedia()
-{
-	//File loading flag
-	bool success{ true };
-
-	//Load splash image
-	std::string imagePath{ "assets/hello-sdl3.bmp" };
-	gHelloWorld = SDL_LoadBMP(imagePath.c_str());
-	if( gHelloWorld == nullptr )
-	{
-		KJK_ERROR("Unable to load image {}! SDL Error: {}\n", imagePath, SDL_GetError());
-		success = false;
-	}
-
-	return success;
-}
-
-//Frees media and shuts down SDL
+//Cleans up and closes SDL and all used objects
 void close()
 {
-	//Clean up surface
-	SDL_DestroySurface(gHelloWorld);
-
 	//Destroy window
-	SDL_DestroyWindow(gWindow);
-	gWindow = nullptr;
-	gScreenSurface = nullptr;
+	if (gWindow != nullptr)
+	{
+		SDL_DestroyWindow(gWindow);
+		gWindow = nullptr;
+	}
 
 	//Quit SDL subsystems
 	SDL_Quit();
